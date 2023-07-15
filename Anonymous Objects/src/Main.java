74461bbb87f4b12d92b24699175d14e03cdb97da
arditcali <129833461+arditcali@users.noreply.github.com>
2023-07-15 11:07:26 +0200

    Add files via upload

---
+++ Anonymous Objects/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        int y = new Object().x + 10;
+        System.out.println(y);
+
+        new Object().show();
+    }
+}
---
+++ Anonymous Objects/src/Object.java
@@ -0,0 +1,9 @@
+public class Object {
+    int x = 10;
+    public void print() {
+        System.out.println(x);
+    }
+    public void show() {
+        System.out.println("Hello World");
+    }
+}
