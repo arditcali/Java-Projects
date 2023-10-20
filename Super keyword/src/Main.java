aaa96712104e79bf6f8cf4a0d3cbad2a86bc0bdd
arditcali <129833461+arditcali@users.noreply.github.com>
2023-10-20 16:33:57 +0200

    Add files via upload

---
+++ Super keyword/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Person clark = new Person("Clark", 40);
+        System.out.println(clark);
+
+        Hero superman = new Hero("Clark", 40, "Strength");
+        System.out.println(superman);
+    }
+}
