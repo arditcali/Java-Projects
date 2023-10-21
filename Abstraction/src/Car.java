cad75eb40f120090e463e6a4a246ebd8e653f3a3
arditcali <129833461+arditcali@users.noreply.github.com>
2023-10-21 22:47:33 +0200

    Add files via upload

---
+++ Abstraction/src/Car.java
@@ -0,0 +1,7 @@
+public class Car extends Start {
+
+    @Override
+    public void go() {
+        System.out.println("Car will start running");
+    }
+}
---
+++ Abstraction/src/Start.java
@@ -0,0 +1,4 @@
+public abstract class Start {
+
+    public abstract void go();
+}
